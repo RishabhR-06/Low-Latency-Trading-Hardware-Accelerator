a2e852d8a852e1ae6d50a8c3a18b6040db4db68e
Rishabh <rishabh.rstgi06@gmail.com>
2025-11-21 18:27:10 +0000

    Added framework for both the square root hardware block and Kalman filter

---
+++ sqr_root.sv
@@ -0,0 +1,17 @@
+module sqr_root #(
+    parameter[5:0] size = 5'd16,
+) (
+    input logic clk,
+    input logic rst,
+    input logic[size-1:0] data_in,
+    output logic[size-1:0] rooted_data_out
+);
+
+
+    //square roots the input data
+    //researching algorithims for now
+
+
+
+    
+endmodule
